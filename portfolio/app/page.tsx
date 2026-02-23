d027d92ff2be55fc4d9ad723a766f26ac738a934
Steven Aguilar <167376361+stevmzz@users.noreply.github.com>
2026-02-22 19:20:01 -0600

    add initial colors and fonts

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "./sections/Hero";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-black text-white">
-      <h1 className="text-5xl font-bold">STEVEN</h1>
+    <main>
+      <Hero />
     </main>
-  );
+  )
 }
---
+++ portfolio/app/sections/Hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <section className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="font-heading text-5xl">STEVEN AGUILAR</h1>
+    </section>
+  );
+}
